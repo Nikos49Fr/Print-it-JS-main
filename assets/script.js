fbcef7da2bdf0fbf824a04389e32dfe9943d0569
Nicolas Grignard <nicolas4980@gmail.com>
2025-07-21 00:51:39 +0200

    Ajout du défilement des images et des bullets au clic sur les flèches

--- assets/script.js
+++ assets/script.js
@@ -17,22 +17,52 @@ const slides = [
 	}
 ]
 
-const leftArrow = document.querySelector(".arrow_left");
-const rightArrow = document.querySelector(".arrow_right");
+//initialisation
+let bulletActive = 0;
+let bulletDesactive = 0;
+// on récupère tout de suite les balises qui seront à modifier
+const bannerImg = document.querySelector(".banner-img");
+const bannerText = document.querySelector("#banner p");
 
-leftArrow.addEventListener("click", () => {
-	console.log("à gauche");
-});
-rightArrow.addEventListener("click", () => {
-	console.log("à droite");
-});
-
+// constructiuon des bullets dots
 const dots = document.querySelector(".dots")
 for (let i = 0; i < slides.length; i++) {
 	let dot = document.createElement("div");
 	dot.classList.add("dot");
-	if (i === 0) {
+	if (i === 0) { // bullet 0 active par défaut
 		dot.classList.toggle("dot_selected");
 	}
 	dots.appendChild(dot);
 }
+
+// fonction principale qui va gérer le changement de slide
+function slideChange(direction) {
+	// on récupère l'ensemble des bullets
+	const dots = document.querySelectorAll(".dot");
+	// on retire la class dot_selected à la bullet active
+	dots[bulletActive].classList.remove("dot_selected");
+	// on détermine dans quel sens le carrousel va défiler
+	if (direction === "left") {
+		bulletActive--;
+	} else if (direction === "right") {
+		bulletActive++;
+	}
+	// on active la bullet de la nouvelle slide
+	dots[bulletActive].classList.add("dot_selected");
+	
+	// on met à jour l'image et le texte correspondant
+	bannerImg.setAttribute("src", `./assets/images/slideshow/${slides[bulletActive].image}`);	
+	bannerText.innerHTML = slides[bulletActive].tagLine;
+}
+
+// EventListener pour la flèche gauche
+const leftArrow = document.querySelector(".arrow_left");
+leftArrow.addEventListener("click", () => {
+	slideChange("left");
+});
+
+// EventListener pour la flèche droite
+const rightArrow = document.querySelector(".arrow_right");
+rightArrow.addEventListener("click", () => {
+	slideChange("right");
+});
